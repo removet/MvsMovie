19813489d01c03f041fad1bf42adf62afe3ece35
orhan <rem0vet.9@gmail.com>
2023-05-29 18:32:19 +0300

    15

---
+++ MvsMovie/Views/HelloWorld/Welcome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvsMovie.Views.HelloWorld
+{
+    public class WelcomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
